dafb3a9d19649be182640d0adc02ced17bbca274
Administrator <Administrator@QMS_PC>
2016-06-25 16:59:47 +0700

    bai26

---
+++ Model/Dao/ProductDao.cs
@@ -0,0 +1,58 @@
+﻿using Model.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Dao
+{
+    public class ProductDao
+    {
+        OnlineShopDbContext db = null;
+
+        public ProductDao()
+        {
+            db = new OnlineShopDbContext();
+        }   
+
+        public List<Product> ListNewProduct(int top)
+        {
+            return db.Product.OrderByDescending(x => x.CreatedDate).Take(top).ToList();
+        }
+
+        /// <summary>
+        /// get list products by category
+        /// </summary>
+        /// <param name="categoryID"></param>
+        /// <returns></returns>
+        public List<Product> ListByCategoryId(long categoryID,ref int totalRecord, int pageIndex = 1,int pageSize = 2)
+        {
+            totalRecord = db.Product.Where(x => x.CategoryID == categoryID).Count();
+            var model = db.Product.Where(x => x.CategoryID == categoryID).Skip((pageIndex-1)*pageSize).Take(pageSize).ToList();
+
+            return model;
+        }
+
+        /// <summary>
+        /// list features product
+        /// </summary>
+        /// <param name="top"></param>
+        /// <returns></returns>
+        public List<Product> ListFeatureProduct(int top)
+        {
+            return db.Product.Where(x => x.TopHot != null && x.TopHot > DateTime.Now).OrderByDescending(x => x.CreatedDate).Take(top).ToList();
+        }
+
+        public List<Product> ListRelatedProduct(long productId)
+        {
+            var product = db.Product.Find(productId);
+            return db.Product.Where(x => x.ID != productId && x.CategoryID == product.CategoryID).ToList();
+        }
+
+        public Product ViewDetail(long id)
+        {
+            return db.Product.Find(id);
+        }
+    }
+}
---
+++ Model/Dao/UserDao.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model.EF;
+using PagedList;
+using PagedList.Mvc;
+
+namespace Model.Dao
+{
+    public class UserDao
+    {
+        OnlineShopDbContext db = null;
+
+        public UserDao()
+        {
+            db= new OnlineShopDbContext();
+        }
+
+        public bool update(User entity)
+        {
+            try
+            {
+                var user = db.User.Find(entity.ID);
+                user.Name = entity.Name;
+                if (!string.IsNullOrEmpty(entity.Password))
+                {
+                    user.Password = entity.Password;
+                }
+                user.Address = entity.Address;
+                user.Email = entity.Email;
+                user.ModifiedBy = entity.ModifiedBy;
+                user.ModifiedDate = DateTime.Now;
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                //logging
+                return false;
+            }
+        }
+
+        public long Insert(User entity)
+        {
+            db.User.Add(entity);
+            db.SaveChanges();
+            return entity.ID;
+        }
+
+        public IEnumerable<User> ListAllPaging(string searchString, int page, int pagesize)
+        {
+            IQueryable<User> model = db.User;
+
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                model =
+                    model.Where(x => x.UserName.Contains(searchString) || x.Name.Contains(searchString));
+            }
+
+            return model.OrderByDescending(x => x.CreatedDate).ToPagedList(page, pagesize);
+        }
+
+        public User GetById(string userName)
+        {
+            return db.User.SingleOrDefault(x=>x.UserName==userName);
+        }
+
+        public User Viewdetail(int id)
+        {
+            return db.User.Find(id);
+        }
+
+        public int Login(string userName, string passWord)
+        {
+            var result = db.User.SingleOrDefault(x => x.UserName == userName && x.Password == passWord);
+            if (result == null)
+            {
+                return 0;
+            }
+            else
+            {
+                if (result.Status == false)
+                {
+                    return -1;
+                }
+                else
+                {
+                    if (result.Password == passWord)
+                        return 1;
+                    else
+                        return -2;
+
+                }
+            }
+        }
+
+        public bool ChangeStatus(long id)
+        {
+            var user = db.User.Find(id);
+            user.Status = !user.Status;
+            db.SaveChanges();
+
+            return user.Status;
+        }
+
+        public bool Delete(int id)
+        {
+            try
+            {
+                var user = db.User.Find(id);
+                db.User.Remove(user);
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+    }
+}
